d826c7ac63ad7f4cabcc979df341b817a31de359
VitaliiSestrenskyi <sestrinskiy@mail.ru>
2017-03-18 19:00:08 +0200

    registry.php
    registry.php

---
+++ registry.php
@@ -0,0 +1,38 @@
+<?
+/**
+ * Class Registry
+ */
+class Registry
+{
+    private static $instance;
+    private $values = array();
+
+    private function __construct() {}
+
+    public static function getInstance()
+    {
+        if( ! isset(self::$instance) )
+            self::$instance = new self();
+
+        return self::$instance;
+    }
+
+    public function get( $key )
+    {
+        if( isset($this->values[$key]) )
+            return $this->values[$key];
+
+        return null;
+    }
+
+    public function set( $key, $value )
+    {
+        $this->values[$key] = $value;
+    }
+}
+
+
+//realization
+$registry = Registry::getInstance();
+$registry->set('MainClass', new MainClass());
+$registry->get('MainClass');
